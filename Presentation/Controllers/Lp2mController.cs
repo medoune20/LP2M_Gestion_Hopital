70279f8cd51f20ca7d316b0ba126a73e311b508c
medoune20 <128698827+medoune20@users.noreply.github.com>
2026-06-27 18:56:12 +0000

    Fix LP2M controller namespace imports

--- Presentation/Controllers/Lp2mController.cs
+++ Presentation/Controllers/Lp2mController.cs
@@ -1,3 +1,4 @@
+using GestionHopital.Domaine;
 using GestionHopital.Infrastructure.Donnees;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -20,10 +21,6 @@ public class Lp2mController : BaseController
         ViewBag.PaiementsMois = await _db.Paiements.Where(p => p.DatePaiement >= debutMois && p.Statut == StatutPaiement.Payé).SumAsync(p => (decimal?)p.Montant) ?? 0;
         ViewBag.AlertesActives = await _db.Alertes.CountAsync(a => a.Statut == StatutAlerte.Active);
         ViewBag.MailsEnvoyes = await _db.JournalMails.CountAsync(m => m.Statut == StatutMail.Envoyé);
-
-        ViewBag.PaiementsRecents = await _db.Paiements.Include(p => p.Patient).OrderByDescending(p => p.DatePaiement).Take(8).ToListAsync();
-        ViewBag.FileRecente = await _db.FileAttente.Include(f => f.Patient).Include(f => f.Medecin).OrderByDescending(f => f.DateArrivee).Take(8).ToListAsync();
-        ViewBag.AlertesRecentes = await _db.Alertes.OrderByDescending(a => a.DateCreation).Take(8).ToListAsync();
 
         return View();
     }
